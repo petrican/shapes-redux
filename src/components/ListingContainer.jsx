7f440c5da047d536e01e206f436922959e4bbd32
Petrica Nanca <petrica.nanca@softvision.ro>
2019-10-11 14:04:33 +0300

    Fix listing shapes for rectangle and oval

--- src/components/ListingContainer.jsx
+++ src/components/ListingContainer.jsx
@@ -7,10 +7,10 @@ const ListingContainer = props => {
   return (
     <div className="listing-container">
         <Shape shape="round" key="1" color="red" />
-        <Shape shape="round" key="2" color="blue" />
-        <Shape shape="round" key="3" color="green" />
-        <Shape shape="round" key="4" color="yellow" />
-        <Shape shape="round" key="5" color="mauve" />
+        <Shape shape="rectangle" key="2" color="blue" />
+        <Shape shape="oval" key="3" color="green" />
+        <Shape shape="triangle" key="4" color="yellow" />
+        <Shape shape="square" key="5" color="mauve" />
     </div>
   );
 };
--- src/components/Shape.jsx
+++ src/components/Shape.jsx
@@ -1,14 +1,61 @@
 import React from "react";
 
 const Shape = props => {
-  return (
-    <div className="shape-container">
-      <div
-        className={`shape-${props.shape} color-${props.color}`}
-        key={props.key}
-      ></div>
-    </div>
-  );
+  let hexColor = "";
+  let formedShape = "";
+
+  switch (props.color) {
+    case "red":
+      hexColor = "#F40101";
+      break;
+    case "gray":
+      hexColor = "#E1E1E1";
+      break;
+    case "green":
+      hexColor = "#18C82B";
+      break;
+    case "blue":
+      hexColor = "#2B9FFF";
+      break;
+    case "yellow":
+      hexColor = "#F2BF44";
+      break;
+    case "mauve":
+      hexColor = "#890087";
+      break;
+  }
+
+  switch (props.shape) {
+    case "triangle":
+      formedShape = (
+        <div
+          className={`shape-triangle`}
+          key={props.key}
+          style={{ borderBottom: `125px solid ${hexColor}` }}
+        ></div>
+      );
+      break;
+    case "rectangle":   // fix position for both rectangle and oval
+    case "oval":
+          formedShape = (
+            <div
+              className={`shape-${props.shape}`}
+              style={{ backgroundColor: `${hexColor}`, marginTop: '37px', marginBottom: '38px' }}
+              key={props.key}
+            ></div>
+          );
+          break;  
+    default:
+      formedShape = (
+        <div
+          className={`shape-${props.shape}`}
+          style={{ backgroundColor: `${hexColor}` }}
+          key={props.key}
+        ></div>
+      );
+  }
+
+  return <div className="shape-container">{formedShape}</div>;
 };
 
 export default Shape;
