15a7116d8881f8cfe18b603121a5c767680325c0
Petrica Nanca <petrica_nanca@yahoo.com>
2019-10-13 05:26:29 +0300

    Deactivate redux devtools

--- src/store.js
+++ src/store.js
@@ -8,6 +8,7 @@ const store = createStore(
         rootReducer, 
         initialState, 
         compose(applyMiddleware(thunk), 
-                window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+                // window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+                ));
 
 export default store;
